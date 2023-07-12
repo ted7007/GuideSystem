4bc1be00878657a2086cacf3f8cb093ec445d61c
BuhalBodyaga <buhalihin1@gmail.com>
2023-07-12 16:37:28 +1000

    Добавляю лабу 1.1 исправил косяки

--- GuideSystemApp/GuideSystemApp/Student/List/Node.cs
+++ GuideSystemApp/GuideSystemApp/Student/List/Node.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Указатели_для_курсовой
+namespace GuideSystemApp.Student.List
 {
     // 1. Объявление типа Указатель
     class Node
